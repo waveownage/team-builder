3387fb7373fc2a4b7236cdd744fc9bafe3016758
Andre Freeman <asf@mail.usf.edu>
2020-03-19 19:16:23 -0400

    Set up formState on App.js an addFormHandler

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [formState, setFormState] = useState([
+    {
+      Id: 1
+      Name: "BoB"
+      Email: "acb.gmail.com"
+      Role:  "Student"
+    }
+  ]);
+
+  const addFormHandler = newForm => {
+    console.log("adding Form", newForm);
+    setFormState([...formState, newForm]);
+  };
+
 }
 
 export default App;
--- team-builder/src/Components/Form.js
+++ team-builder/src/Components/Form.js
@@ -2,4 +2,3 @@ import React, { useState } from "react";
 
 import { withFormik, Form, Field } from "formik";
 import * as Yup from "yup";
-
