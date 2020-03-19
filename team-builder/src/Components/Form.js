7a05122e11cad6f59f08251c75cc5b3979ce3f83
Andre Freeman <asf@mail.usf.edu>
2020-03-19 19:21:22 -0400

    Created Form on Form.js and Map Method

--- team-builder/src/Components/Form.js
+++ team-builder/src/Components/Form.js
@@ -1,4 +1,22 @@
 import React, { useState } from "react";
-
 import { withFormik, Form, Field } from "formik";
 import * as Yup from "yup";
+
+
+
+
+const Form = props => {
+    return (
+      <div className="form-list">
+        {props.Form.map(member => (
+          <div className="team-member" key={member.id}>
+            <h2>{member.name}</h2>
+            <p>{member.email}</p>
+            <p>{member.role}</p>
+          </div>
+        ))}
+      </div>
+    );
+  };
+
+  export default Form;
